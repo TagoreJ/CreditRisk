dd87038c147288010f78e2771c5429111430340f
Ekalavya Prasad <eklavya.prasad2709@gmail.com>
2024-11-20 21:14:14 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -404,7 +404,7 @@ with st.sidebar:
 
 
 if st.button('Build profile'):
-    if prob = None:
+    if prob == None:
         st.warning('⚠️ Please fill out the form in its entirety')
         
     prob = get_probability(input_df)
