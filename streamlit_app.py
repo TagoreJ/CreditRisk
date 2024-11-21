ed257ecf085a9f5a2203d5e206feea972b9d465a
Ekalavya Prasad <eklavya.prasad2709@gmail.com>
2024-11-21 11:37:13 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,13 +5,13 @@ st.title('💳 Credit risk checker')
 st.info('👈 Please fill your profile in the sidebar')
 
 st.expander('⚠️PLEASE READ BEFORE PROCEEDING'):
-    st.write('For Testing Purposes Only: This app is a prototype designed for testing and educational purposes. The results provided should not be used for any official or financial decision-making.')
+expander.write('For Testing Purposes Only: This app is a prototype designed for testing and educational purposes. The results provided should not be used for any official or financial decision-making.')
 
-    st.write('Model Accuracy vs. Data Limitations: While the underlying deep learning model achieves a high accuracy of 83% (as documented in my dissertation), it was trained on outdated data sourced from the UCI Machine Learning Repository. This limits its ability to fully reflect current real-world credit risk scenarios.')
+expander.write('Model Accuracy vs. Data Limitations: While the underlying deep learning model achieves a high accuracy of 83% (as documented in my dissertation), it was trained on outdated data sourced from the UCI Machine Learning Repository. This limits its ability to fully reflect current real-world credit risk scenarios.')
 
-    st.write('Not Financial Advice: The apps outputs do not constitute financial advice. Please consult a certified financial professional for decisions regarding your credit or finances.')
+expander.write('Not Financial Advice: The apps outputs do not constitute financial advice. Please consult a certified financial professional for decisions regarding your credit or finances.')
 
-    st.write('Learn More: For more detailed information about the app backend and methodology, you can access my dissertation.')
+expander.write('Learn More: For more detailed information about the app backend and methodology, you can access my dissertation.')
 
 
 
